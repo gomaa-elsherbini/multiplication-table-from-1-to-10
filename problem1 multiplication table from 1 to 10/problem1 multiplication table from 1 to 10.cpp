1f97db09ff2e041f85a5a243b1117c05e34c458b
gomaa <gogochemist35@gmail.com>
2023-09-12 19:19:00 +0300

    edit some codes

--- problem1 multiplication table from 1 to 10/problem1 multiplication table from 1 to 10.cpp
+++ problem1 multiplication table from 1 to 10/problem1 multiplication table from 1 to 10.cpp
@@ -6,35 +6,35 @@ using namespace std;
 
 void tableHeader()
 {
-    cout << "\t\t\tMultiplication table from 1 to 10\t\n\n";
-    
+    cout << "\n\n\t\t\tMultiplication table from 1 to 10\t\n\n";
+    cout << "\t";
     for (int i = 1; i <= 10; i++)
     {
-        if (i == 1) {
-            cout << "\t";
-        }
-        cout << "\t" << i;
+        cout << i << "\t";
     }
-    cout << "\n-------------------------------------------------------------------------------------------\n";
+    cout << "\n-----------------------------------------------------------------------------------\n";
 }
 
-void tableSide()
+string columnSeparator(int i){
+    if (i <= 9)
+        return "  |";
+    else
+        return " |";
+}
+
+void printMultiplicationTableFrom1To10()
 {
-    
+    tableHeader();
+
     for (int i = 1; i <= 10; i++)
     {
-        cout <<"   "<< i << "\t|";
+        cout <<" "<< i << columnSeparator(i)<<"\t";
 
         for (int x = 1; x <= 10; x++)
         {
-            if (x != 10) {
-                cout << "\t" << i * x;
-            }
-            else
-            {
-                cout << "\t" << i * x << "\n";
-            }
+                cout << i * x << "\t";
         }
+        cout << endl;
     }
 
 }
@@ -43,6 +43,5 @@ void tableSide()
 
 int main()
 {
-   tableHeader();
-   tableSide();
+    printMultiplicationTableFrom1To10();
 }
